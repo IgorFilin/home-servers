9125ce95cbd62ec14bc5f471438eee0bc9dae7a1
filin_ubuntu <filinigor@yandex.ru>
2025-10-26 13:14:29 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/KnUtYCZePq

---
+++ apps/home-servers/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'home-servers',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/home-servers',
+};
