1c949e5b4568fb0b702aacc43c7b701d347df9cd
filin_ubuntu <filinigor@yandex.ru>
2025-11-02 19:24:32 +0300

    Добавление константы типизации ошибок

--- apps/gateway/src/gateway/gateway.controller.ts
+++ apps/gateway/src/gateway/gateway.controller.ts
@@ -8,6 +8,7 @@ import {
 import { GatewayService } from './gateway.service';
 import { catchError, EMPTY, Observable, tap, timeout } from 'rxjs';
 import { RegistrationDto } from '@home-servers/shared';
+import { ERROR_EXEPTION } from '../types';
 
 @Controller()
 export class GatewayController {
@@ -17,9 +18,8 @@ export class GatewayController {
   registration(@Body() regBody: RegistrationDto): Observable<any> {
     return this.gatewayService.registration(regBody).pipe(
       catchError((error) => {
-        console.log('В ОШИБКЕ ============================');
         throw new HttpException(
-          error.message || 'Registration failed',
+          error.message || ERROR_EXEPTION.REGISTRATION,
           error.statusCode || HttpStatus.INTERNAL_SERVER_ERROR
         );
       })
---
+++ apps/gateway/src/types/enums.ts
@@ -0,0 +1,3 @@
+export const ERROR_EXEPTION = {
+    REGISTRATION: "Ошибка регистрации"
+} as const
